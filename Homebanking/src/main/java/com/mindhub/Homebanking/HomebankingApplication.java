517120caf6302927c876cae2bf591a94c362092e
monica <monibrave1@gmail.com>
2023-08-03 14:33:51 -0300

    first commit

---
+++ Homebanking/src/main/java/com/mindhub/Homebanking/HomebankingApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.Homebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+}
